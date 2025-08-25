13d99ac37017ebfcfeb685431a7074c642d46bcb
IPampurin <I.Pampurin@yandex.ru>
2025-08-25 18:48:33 +0500

    modify handlers and db

--- L0/db/db.go
+++ L0/db/db.go
@@ -1,6 +1,65 @@
 package db
 
-func ConnectDB() error {
+import (
+	"fmt"
+	"log"
+	"os"
 
-	return nil
+	"github.com/IPampurin/WB-technical-schools/L0/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDB() {
+
+	// создаём URL для соединения с базой данных.
+	// Имя пользователя базы данных, пароль и имя базы данных, а также порт базы берутся из
+	// переменных окружения, они описаны в файле .env
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
+		os.Getenv("DBL0_USER"),
+		os.Getenv("DBL0_PASSWORD"),
+		os.Getenv("DBL0_NAME"),
+		os.Getenv("DBL0_PORT"),
+	)
+	// создаём подключение к базе данных.
+	// В &gorm.Config настраивается логер, который будет сохранять информацию
+	// обо всех активностях с базой данных.
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Не удалось подключиться к базе данных: ", err)
+		os.Exit(1)
+	}
+
+	log.Println("Подключение к базе данных установлено.")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Запуск миграций")
+	// собираем модели в срез
+	models := []interface{}{
+		&models.Order{},
+		&models.Delivery{},
+		&models.Payment{},
+		&models.Item{},
+	}
+	// автомиграцию моделей выполняем списком
+	err = db.AutoMigrate(models...)
+	if err != nil {
+		log.Fatalf("Ошибка при выполнении миграций: %v", err)
+	}
+	log.Println("Миграции успешно применены.")
+
+	DB = Dbinstance{
+		Db: db,
+	}
 }
--- L0/handlers/handlers.go
+++ L0/handlers/handlers.go
@@ -1,5 +1,103 @@
 package handlers
 
-func main() {
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
 
+	"github.com/IPampurin/WB-technical-schools/L0/db"
+	"github.com/IPampurin/WB-technical-schools/L0/models"
+	"github.com/go-chi/chi/v5"
+)
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	orders := []models.Order{}
+
+	resultDB := db.DB.Db.Find(&orders)
+	if resultDB.Error != nil {
+		http.Error(w, resultDB.Error.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp, err := json.Marshal(orders)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func PostTask(w http.ResponseWriter, r *http.Request) {
+	orders := new(models.Order)
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &orders); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println(orders)
+
+	resultDB := db.DB.Db.Create(&orders)
+	if resultDB.Error != nil {
+		http.Error(w, resultDB.Error.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func GetTaskByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	order := new(models.Order)
+
+	resultDB := db.DB.Db.First(&order, id)
+	if resultDB.Error != nil {
+		http.Error(w, resultDB.Error.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp, err := json.Marshal(order)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resultDB := db.DB.Db.Delete(&models.Order{}, id)
+	if resultDB.Error != nil {
+		http.Error(w, resultDB.Error.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 }
