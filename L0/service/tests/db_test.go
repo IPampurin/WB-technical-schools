a03dfff292429708e0532ba84ae750112dd6766a
IPampurin <I.Pampurin@yandex.ru>
2025-09-10 17:33:52 +0500

    all

--- L0/service/tests/db_test.go
+++ L0/service/tests/db_test.go
@@ -27,7 +27,7 @@ func openTestDB(t *testing.T) *gorm.DB {
 	}
 	nameDB, ok := os.LookupEnv("DBL0_NAME")
 	if !ok {
-		nameDB = "level_zero_db"
+		nameDB = "level-zero-db"
 	}
 	passwordDB, ok := os.LookupEnv("DBL0_PASSWORD")
 	if !ok {
--- L0/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ./kafka/compose.yml up -d
-docker-compose -f ./producer/compose.yml up -d
-docker-compose -f ./service/compose.yml up -d
-docker-compose -f ./consumer/compose.yml up -d
-docker-compose -f ./redisCache/compose.yml up -d
