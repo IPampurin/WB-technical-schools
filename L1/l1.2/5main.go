297b17a778b1060b259b638cbe287bbcaac0e95b
IPampurin <I.Pampurin@yandex.ru>
2025-09-13 21:23:37 +0500

    l1.2 in progress

--- L1/l1.2/5main.go
+++ L1/l1.2/5main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -11,25 +12,31 @@ func main() {
 	// входные данные
 	input := []int{2, 4, 6, 8, 10}
 
+	// создаём канал с буфером по длине входных данных
 	ch := make(chan int, len(input))
 
 	// задаём WaitGroup
-	//var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
 	// запускаем len(input) горутин
 	for i := 0; i < len(input); i++ {
-		//	wg.Add(1)
-		go func(idx int) {
-			//		defer wg.Done()
-			ch <- input[idx] // отправляем элемент в канал
-		}(i)
+		wg.Add(1) // прибавляем счётчик WaitGroup
+		go func() {
+			defer wg.Done() // по закрытии горутины вычитаем счётчик WaitGroup
+			// смотрим, что есть в канале и печатаем квадрат этого числа
+			for v := range ch {
+				fmt.Printf("%d ", v*v)
+			}
+		}()
 	}
-	//wg.Wait()
 
-	close(ch) // закрываем канал
-
-	// печатаем квадрат числа, вычитанного из канала
-	for v := range ch {
-		fmt.Printf("%d ", v*v)
+	// отправляем числа из массива в канал
+	for i := 0; i < len(input); i++ {
+		ch <- input[i]
 	}
+
+	// закрываем канал
+	close(ch)
+	// ждём окончания работы всех горутин
+	wg.Wait()
 }
---
+++ L1/l1.2/6main.go
@@ -0,0 +1,40 @@
+/* ВАРИАНТ №6 - решение с использованием пула воркеров и хаотичным порядком вывода */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// входные данные
+	input := []int{2, 4, 6, 8, 10}
+
+	// создаём канал, куда будем писать элементы из input
+	jobChan := make(chan int, len(input))
+
+	// создаём канал, из которого будем забирать квадраты чисел
+	resChan := make(chan int, len(input))
+
+	// запускаем len(input) горутин (пул воркеров)
+	for i := 0; i < len(input); i++ {
+		go func(jobs <-chan int, res chan<- int) {
+			for v := range jobs {
+				res <- v * v
+			}
+		}(jobChan, resChan)
+	}
+
+	// отправляем числа из массива в канал jobChan
+	for i := 0; i < len(input); i++ {
+		jobChan <- input[i]
+	}
+	// закрываем канал jobChan
+	close(jobChan)
+
+	// получаем len(input) чисел из канала с результатами и печатаем их
+	for i := 0; i < len(input); i++ {
+		fmt.Printf("%d ", <-resChan)
+	}
+	// для порядка закрываем канал resChan
+	close(resChan)
+}
