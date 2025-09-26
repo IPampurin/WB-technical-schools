43b78015a92ab442b344edfc54bef3c7ad47ff06
IPampurin <I.Pampurin@yandex.ru>
2025-09-26 18:18:26 +0500

    main.go ready l1.8

--- L1/l1.8/main.go
+++ L1/l1.8/main.go
@@ -10,9 +10,11 @@ func SetBit(value int64, i int, bitValue int) int64 {
 
 	if bitValue == 1 {
 		// установка бита в 1 с помощью OR
+		// с учётом условия, что младший бит не нулевой, а первый
 		return value | (1 << (i - 1))
 	} else {
 		// установка бита в 0 с помощью AND NOT
+		// с учётом условия, что младший бит не нулевой, а первый
 		return value &^ (1 << (i - 1))
 	}
 }
@@ -20,8 +22,21 @@ func SetBit(value int64, i int, bitValue int) int64 {
 func main() {
 
 	var number int64 = 5    // входящее число в десятичном формате
-	var bitPosition int = 4 // нумеруем биты с номера один, справа налево
-	var newBitValue int = 1 // или 0 или 1
+	var bitPosition int = 1 // нумеруем биты с номера один (по условию задачи), справа налево
+	var newBitValue int = 0 // или 0 или 1
+
+	// валидируем bitPosition
+	if uint(bitPosition-1) > 63 {
+		// (bitPosition - 1) преобразуем в uint (проверяем на отрицательность)
+		// если результат > 63, значит исходный bitPosition > 64
+		fmt.Println("bitPosition должен быть в пределах от 1 до 64")
+		return
+	}
+	// побитовая проверка newBitValue
+	if newBitValue&^1 != 0 { // newBitValue&^1 != 0 значит не 0 и не 1
+		fmt.Println("newBitValue должен быть или 0, или 1")
+		return
+	}
 
 	fmt.Printf("\nИсходное число:\n	- в десятичной форме %d\n	- в двоичном формате %064b\n", number, number)
 	fmt.Printf("Позиция бита, который меняем: %d\n", bitPosition)
