7110312fa2a255d8acd7fe818245ac070608a6ce
IPampurin <I.Pampurin@yandex.ru>
2025-10-08 15:15:05 +0500

    add l2.2

---
+++ L2/l2.2/main.go
@@ -0,0 +1,30 @@
+/* ВАРИАНТ №1 - решение задачи l2.2 */
+
+package main
+
+import "fmt"
+
+func test() (x int) {
+
+	defer func() {
+		x++
+	}()
+	x = 1
+	return
+}
+
+func anotherTest() int {
+
+	var x int
+	defer func() {
+		x++
+	}()
+	x = 1
+	return x
+}
+
+func main() {
+
+	fmt.Println(test())
+	fmt.Println(anotherTest())
+}
