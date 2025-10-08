885b7292d87f2b0348c5ad05d15914962b8e9d24
IPampurin <I.Pampurin@yandex.ru>
2025-10-08 17:42:54 +0500

    add l2.3

---
+++ L2/l2.3/main.go
@@ -0,0 +1,19 @@
+/* ВАРИАНТ №1 - решение задачи l2.3 */
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func Foo() error {
+	var err *os.PathError = nil
+	return err
+}
+
+func main() {
+	err := Foo()
+	fmt.Println(err)
+	fmt.Println(err == nil)
+}
