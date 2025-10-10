7a5c3c9ed67a9b22dae3e309178059cb54a955e9
IPampurin <I.Pampurin@yandex.ru>
2025-10-10 21:26:46 +0500

    main.go ready l2.5

--- L2/l2.5/main.go
+++ L2/l2.5/main.go
@@ -2,24 +2,36 @@
 
 package main
 
+// customError кастомный тип ошибки
 type customError struct {
 	msg string
 }
 
+// Error метод реализующий интерфейс error для типа customError
 func (e *customError) Error() string {
 	return e.msg
 }
 
+// test возвращает указатель на тип customError со значением nil
 func test() *customError {
 	// ... do something
 	return nil
 }
 
 func main() {
+
+	// объявляем ошибку err (интерфейс error)
 	var err error
+
+	// присваиваем переменной err результат функции test(),
+	// то есть значение nil типа *customError
 	err = test()
+
+	// пытаемся сравнить с nil переменную интерфейса error,
+	// у которой значение nil, а тип - *customError.
+	// интерфейс не равен nil, потому что у него есть тип
 	if err != nil {
-		println("error")
+		println("error") // вывод программы: error
 		return
 	}
 	println("ok")
