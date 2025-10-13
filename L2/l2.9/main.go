f343021d5aed519b9122b83de2bb61f8dd85c54f
IPampurin <I.Pampurin@yandex.ru>
2025-10-13 18:16:34 +0500

    l2.9 in progress

--- L2/l2.9/main.go
+++ L2/l2.9/main.go
@@ -2,10 +2,34 @@
 
 package main
 
-func unpackingString() {
+import "fmt"
 
+func UnpackingString(str string) (string, error) {
+
+	result := ""
+
+	return result, nil
 }
 
 func main() {
 
+	input := []string{
+		"a4bc2d5e",  // "aaaabccddddde"
+		"abcd",      // "abcd"
+		"45",        // "" + err (некорректная строка, т.к. в строке только цифры — функция должна вернуть ошибку)
+		"",          // "" (пустая строка -> пустая строка)
+		"qwe\\4\\5", // "qwe45" (4 и 5 не трактуются как числа, т.к. экранированы)
+		"qwe\\45",   // "qwe44444" (\4 экранирует 4, поэтому распаковывается только 5)
+		"\\45",      // "44444"
+		"\\",        // ""
+	}
+
+	for _, v := range input {
+		resUnpack, err := UnpackingString(v)
+		if err != nil {
+			fmt.Printf("Строка: %10s,  ошибка: %v\n", v, err)
+			continue
+		}
+		fmt.Printf("Строка: %10s,  распаковка: %s\n", v, resUnpack)
+	}
 }
--- L2/l2.9/main_test.go
+++ L2/l2.9/main_test.go
@@ -1,11 +1,44 @@
-/* ВАРИАНТ №1 - решение задачи l2.9, функция unpackingString() */
+package tests
 
-package main
+import (
+	"testing"
+)
 
-func unpackingString() {
-
+func TestUnpackingString(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := UnpackingString(tt.args.str)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UnpackingString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("UnpackingString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func main() {
+/*
 
-}
+	"a4bc2d5e",  // "aaaabccddddde"
+	"abcd",      // "abcd"
+	"45",        // "" + err (некорректная строка, т.к. в строке только цифры — функция должна вернуть ошибку)
+	"",          // "" (пустая строка -> пустая строка)
+	"qwe\\4\\5", // "qwe45" (4 и 5 не трактуются как числа, т.к. экранированы)
+	"qwe\\45",   // "qwe44444" (\4 экранирует 4, поэтому распаковывается только 5)
+	"\\45",      // "44444"
+	"\\",        // ""
+
+*/
